41b9f7b9ea58881383fa2ab60a740313c2454751
Joeshu <1442702103@qq.com>
2020-09-29 17:37:18 +0800

    🎨 在微信小程序中将组件节点渲染为虚拟节点，更加接近Vue组件的表现

--- components/i-ui/mixins/component.js
+++ components/i-ui/mixins/component.js
@@ -1,5 +1,10 @@
 
 export default {
+    // #ifdef MP
+    options: {
+        virtualHost: true // 在微信小程序中将组件节点渲染为虚拟节点，更加接近Vue组件的表现
+    },
+    // #endif
     props: {
         customStyle: {
             type: Object,
