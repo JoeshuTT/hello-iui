e965dceb4006f8214af5f5a842c15cd908a9b479
Joeshu <1442702103@qq.com>
2021-04-07 18:02:20 +0800

    feat

--- components/i-ui/i-sku/i-sku.vue
+++ components/i-ui/i-sku/i-sku.vue
@@ -1,5 +1,20 @@
 <template>
-  <view class="i-sku" :class="[customClass]"> Hi </view>
+  <i-popup v-model="show" position="bottom" :custom-style="mergeStyle"  >
+    <view class="sku-container">
+<view class="sku-header">
+  header
+</view>
+<view class="sku-body">
+  body
+</view>
+<view class="sku-action">
+  action
+</view>
+    </view>
+    <view class="sku-close">
+      <i-icon size="32" color="#323233" class="sku-close-icon" name="cross" />
+    </view>
+  </i-popup>
 </template>
 
 <script>
@@ -10,9 +25,28 @@ export default {
   // },
   // #endif
   props: {
+    show: {
+      type: Boolean,
+      default: true,
+    },
     customClass: {
       type: String,
       default: '',
+    },
+  },
+  computed:{
+mergeStyle() {
+      const { customStyle } = this
+
+      const style = {
+        // #ifdef H5
+        height:'80vh',
+        // #endif
+        'background-color': '#fff',
+        'z-index': 999, 
+      }
+
+      return Object.assign({}, style, customStyle)
     },
   },
   methods: {
